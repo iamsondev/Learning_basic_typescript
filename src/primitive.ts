7cad5c0425f1e874c8d5874296d0267ab92c2079
iamsondev <sondipkumarsk@gmail.com>
2025-11-11 00:27:40 +0600

    primitive of typescript

---
+++ src/primitive.ts
@@ -0,0 +1,10 @@
+let userName: string = "Sondip Kumar";
+let userId = 35364;
+
+userName.split;
+userId.toExponential;
+
+let isAdmin: boolean = true;
+isAdmin = true;
+
+let x: undefined = undefined;
