7b7d5988d09a0bfddf62c83aa5c98d9e6bbfc205
iamsondev <sondipkumarsk@gmail.com>
2025-11-11 13:03:49 +0600

    refrence type object

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -11,3 +11,20 @@ let coordinates: [number, number] = [20, 30];
 let couple: [string, string] = ["Husband", "wife"];
 
 let destination: [string, string, number] = ["Dinajpur", "Haripur", 70];
+
+// reference type object
+const user: {
+  // organization: "Alokanonda";
+  readonly organization: string;
+  firstName: string;
+  middleName?: string;
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "Alokanonda",
+  firstName: "Sondip",
+  lastName: "Kumar",
+  isMarried: false,
+};
+
+console.log(user);
