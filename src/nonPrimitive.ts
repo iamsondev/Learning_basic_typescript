4d6b3cf48a02308dd57ffd1f0d66c975c56b4481
iamsondev <sondipkumarsk@gmail.com>
2025-11-11 12:23:08 +0600

    Explorimg non Primitive

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,13 @@
+//  array, object
+
+let stationary: string[] = ["Pen", "Pencil", "Ball", "Soap"];
+stationary.push("Dairy");
+
+let mixedArray: (string | number)[] = ["Pen", 12, "Pencil", 78, "Soap"];
+mixedArray.push(true); //use only string and number
+
+//tuple
+let coordinates: [number, number] = [20, 30];
+let couple: [string, string] = ["Husband", "wife"];
+
+let destination: [string, string, number] = ["Dinajpur", "Haripur", 70];
